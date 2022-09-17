f72e20906d2372ed7169aec2e25bcc6bdd53929c
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-17 12:37:04 -0400

    create function in object and then call it

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,7 +11,6 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
   openingHours: {
     thu: {
       open: 12,
@@ -26,9 +25,9 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]]
+  },
 };
 
-const arr = [1, 2, 3]
-const [x, y, z] = arr
-console.log(x, y, z)
-console.log(arr)
+console.log(restaurant.order(2, 0))
