7770df496bb9d1a28263a7582887fe8aed274f82
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-19 08:45:45 -0400

    mutate variables and do example with nested objects

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -48,3 +48,16 @@ const {
 } = restaurant
 
 console.log(restaurantName, hours, tags)
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 14 };
+({ a, b } = obj);
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
