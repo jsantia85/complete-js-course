9390304c3db08db5ced59d1720267224774ce597
JASantia85 <JuanASantiago1058@gmail.com>
2022-09-19 08:50:45 -0400

    work with spread operator

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -61,3 +61,18 @@ const {
   fri: { open: o, close: c },
 } = openingHours;
 console.log(o, c);
+
+// The Spread Operator (...)
+
+const arr = [7, 8, 9];
+const badNewArr = [1, 2, arr[0], arr[1], arr[2]];
+console.log(badNewArr);
+
+const newArr = [1, 2, ...arr];
+console.log(newArr);
+
+console.log(...newArr);
+console.log(1, 2, 7, 8, 9);
+
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+console.log(newMenu);
